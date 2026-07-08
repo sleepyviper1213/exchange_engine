1184d44971f5b8fd9a01a4a3e8f571c8b601b25c
Nguyễn Khắc Trường <johnkt12qz@gmail.com>
2026-07-08 21:36:35 +0700

    [style] clang-format

--- src/utils/start_lifetime_as.hpp
+++ src/utils/start_lifetime_as.hpp
@@ -1,17 +1,18 @@
 #pragma once
-#include <type_traits>
-#include <memory>
+
 #include <cstring>
+#include <new>
+#include <type_traits>
+
 namespace utils {
-// Posted by user17732522, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-07-07, License - CC BY-SA 4.0
-template<class T>
+// Posted by user17732522, modified by community. See post 'Timeline' for change
+// history Retrieved 2026-07-07, License - CC BY-SA 4.0
+template <class T>
 	requires (std::is_trivially_copyable_v<T>)
 [[nodiscard]]
 T *start_lifetime_as_array(void *p, std::size_t n) noexcept {
-	if (n == 0)
-		return static_cast<T *>(p);
+	if (n == 0) return static_cast<T *>(p);
 
 	return std::launder(static_cast<T *>(std::memmove(p, p, sizeof(T) * n)));
 }
-}
+} // namespace utils
