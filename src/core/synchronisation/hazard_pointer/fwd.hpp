33d3df39bb735c789710b254dea57d9bc0cc929e
Nguyễn Khắc Trường <johnkt12qz@gmail.com>
2026-07-22 17:34:22 +0700

    [feat] implement hazard pointer

---
+++ src/core/synchronisation/hazard_pointer/fwd.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstddef>
+#include <memory>
+
+namespace core::synchronisation {
+
+template <class T, class D = std::default_delete<T>>
+class hazard_pointer_obj_base;
+
+class hazard_pointer;
+class hazard_pointer_domain;
+
+hazard_pointer make_hazard_pointer();
+hazard_pointer make_hazard_pointer(hazard_pointer_domain &);
+void           swap(hazard_pointer &, hazard_pointer &) noexcept;
+
+template <std::size_t N = 1>
+class hazard_pointer_array;
+
+} // namespace core::synchronisation
