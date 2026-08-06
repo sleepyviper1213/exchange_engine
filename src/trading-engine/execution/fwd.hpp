7c24af2409beccf727cdde591556ed124f78b168
Nguyễn Khắc Trường <johnkt12qz@gmail.com>
2026-08-06 11:54:42 +0700

    [build] Add windows-*-relwithdebinfo preset

--- src/trading-engine/execution/fwd.hpp
+++ src/trading-engine/execution/fwd.hpp
@@ -1,19 +1,16 @@
 #pragma once
 
-#include "trading_engine_export.hpp"
-
 #include <bit>
 #include <cstddef>
 
 namespace exchange::engine::execution {
 
-
 template <std::size_t QueueCapacity = 1U << 14>
 	requires (std::has_single_bit(QueueCapacity))
 class engine_partition;
 
-class TRADING_ENGINE_EXPORT book_manager;
-class TRADING_ENGINE_EXPORT dispatcher;
-class TRADING_ENGINE_EXPORT matching_engine;
+class book_manager;
+class dispatcher;
+class matching_engine;
 
 } // namespace exchange::engine::execution
